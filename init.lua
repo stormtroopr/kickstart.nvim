41e658f8c27f1dbf875ead6f938d466d5ec2a9db
Marcus Le Count <marcus@landsurveys.net.au>
2025-02-14 12:26:27 +0800

    add csharp support

--- init.lua
+++ init.lua
@@ -939,7 +939,23 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'javascript', 'typescript', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'javascript',
+        'terraform',
+        'c_sharp',
+        'typescript',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
---
+++ lua/custom/plugins/lsp.lua
@@ -0,0 +1,26 @@
+return {
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+  },
+  config = function()
+    require('mason').setup()
+    require('mason-lspconfig').setup {
+      ensure_installed = { 'terraformls', 'tflint', 'omnisharp' },
+    }
+
+    local lspconfig = require 'lspconfig'
+
+    -- Setup LSPs
+    lspconfig.terraformls.setup {}
+    lspconfig.tflint.setup {}
+    lspconfig.omnisharp.setup {
+      cmd = {
+        'dotnet',
+        vim.fn.expand '~/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll',
+      },
+      enable_editorconfig_support = true,
+    }
+  end,
+}
